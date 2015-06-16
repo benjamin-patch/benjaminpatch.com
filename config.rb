a297fe5d7c2e810d2a0b9e11f7b93410a55749be
Benjamin Patch <ben@cascadingmedia.com>
2015-06-16 10:18:37 -0500

    Prompt IE9 (and older) users to upgrade their browser.

--- config.rb
+++ config.rb
@@ -73,9 +73,6 @@ configure :build do
   # Enable gzip
   activate :gzip
 
-  # Minify HTML on build
-  activate :minify_html, remove_input_attributes: false
-
   # For example, change the Compass output style for deployment
   activate :minify_css
 
@@ -90,4 +87,23 @@ configure :build do
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+
+  activate :minify_html do |html|
+    html.remove_multi_spaces        = true   # Remove multiple spaces
+    html.remove_comments            = true   # Remove comments
+    html.remove_intertag_spaces     = false  # Remove inter-tag spaces
+    html.remove_quotes              = true   # Remove quotes
+    html.simple_doctype             = false  # Use simple doctype
+    html.remove_script_attributes   = true   # Remove script attributes
+    html.remove_style_attributes    = true   # Remove style attributes
+    html.remove_link_attributes     = true   # Remove link attributes
+    html.remove_form_attributes     = false  # Remove form attributes
+    html.remove_input_attributes    = true   # Remove input attributes
+    html.remove_javascript_protocol = true   # Remove JS protocol
+    html.remove_http_protocol       = false  # Remove HTTP protocol
+    html.remove_https_protocol      = false  # Remove HTTPS protocol
+    html.preserve_line_breaks       = false  # Preserve line breaks
+    html.simple_boolean_attributes  = true   # Use simple boolean attributes
+    # html.preserve_patterns          = nil    # Patterns to preserve
+  end
 end
